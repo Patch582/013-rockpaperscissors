ed2f72c91c6c4e8cd82eaef07a17161180a66183
Pat Booth <patrickbooth582@gmail.com>
2019-06-02 16:51:19 +1000

    Created game and print_header functions

---
+++ game.py
@@ -0,0 +1,13 @@
+
+def print_header():
+    print('----------------------------------')
+    print('       Rock Paper Scissors')
+    print('----------------------------------')
+    print('')
+
+def game():
+    print_header()
+
+
+if __name__ == '__main__':
+    game()
